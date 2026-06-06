32ab41c46dc4ec12903b46c99d1f19724e7d4b02
abderrahmaneRabeh <rabehlife144@gmail.com>
2026-06-06 11:38:48 +0100

    create the frame the statr to add the inputs and their styles

--- index.py
+++ index.py
@@ -1,26 +1,56 @@
 from tkinter import *
 
-class Student:
+
+class Courrier:
     def __init__(self, root):
         self.root = root
-        self.root.geometry('1250x720')
-        self.root.title('Gestion des Archives')
+        # self.root.geometry('1440x900')
+        self.root.geometry(
+            f"{self.root.winfo_screenwidth()}x{self.root.winfo_screenheight()}"
+        )
+        # This is called an f-string (formatted string).
+        # winfo_screenwidth() → gets your screen width
+        # winfo_screenheight() → gets your screen height
+        # so the window adapts automatically
+        self.root.title("Gestion des Archives")
         self.root.configure(background="#7AE2CF")
-        self.root.resizable(False,False)
-        title = Label(self.root, 
+        self.root.resizable(False, False)
+        title = Label(
+            self.root,
             text='systeme de l"archivage',
-            bg='#077A7D',
-            fg='white',
-            font=('monospace',12,'bold')
+            bg="#077A7D",
+            fg="white",
+            font=("monospace", 12, "bold"),
         )
         title.pack(fill=X)
-        
-        
+
+        Manage_Frame = Frame(self.root, bg="white")
+        Manage_Frame.place(x=1100, y=25, width=450, height=850)
+
+        # inputs
+        Num_Msg = Label(
+            Manage_Frame,
+            text="Numéro du message",
+            bg="#007979",
+            fg="white",
+            font=("Helvetica", 11, "bold"),
+            padx=10,
+            pady=5,
+        )
+        Msg_input = Entry(
+            Manage_Frame,
+            bd=2,
+            font=("Helvetica", 11),
+            width=30,
+            relief=SOLID,
+        )
+        Num_Msg.pack(pady=(10, 5), fill=X)
+        Msg_input.pack(padx=10, pady=(0, 15))
 
 
 root = Tk()
 
-Std_Obj = Student(root)
+Courrier_Obj = Courrier(root)
 
 root.mainloop()
 
